cf15de2fc288bca615e1575da74d63428dd8b39f
Josh Hartwig <joshman211@gmail.com>
2022-05-12 20:58:42 -0500

    built out init structure

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/*
+TODO: Determine how to style the divs so they are in a grid with flexbox
+TODO: Create a function that creates the divs on page load 16x16 grid each div class of grid with unique id
+TODO: Create a function that determines which grid you are hovering over and onclick() mark the grid / toggle a style black background
+TODO: The backfill color should be selectable 
+TODO: Grid size should be adjustable support up to 64
+*/
+// create 16 divs
+function createDivs(){
+
+}
