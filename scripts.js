f3f69a0b9ceeb3647904959841558847e09a0125
Josh Hartwig <joshman211@gmail.com>
2022-05-13 19:32:40 -0500

    fleshed out createDivs function still needs work.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,27 @@ TODO: The backfill color should be selectable
 TODO: Grid size should be adjustable support up to 64
 */
 // create 16 divs
-function createDivs(){
+function createDivs(rows, cols, container){
+    const cont = document.querySelector(`#${container}`);
+    let divs = [];
+    for (let x = 0; x < rows; x++) {
+      let row = document.createElement('div');
+      row.classList.add(`row${x}`);
+      
+      for(let y = 0; y < cols; y++ ) {
+        let col = document.createElement('div');
+        col.classList.add(`r${x}`);
+        col.classList.add(`c${y}`);
+        col.style.cssText = 'outline: 1px dashed #f90; width: 20px; height: 20px';
+        row.appendChild(col);
+      };
 
-}
+      divs.push[row];
+    };
+
+    divs.forEach(e => {
+        cont.appendChild(e);
+    });  
+ }
+
+
