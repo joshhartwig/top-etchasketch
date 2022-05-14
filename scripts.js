0e524099a2340b291005490dcd01cbdbe0c79a35
Josh Hartwig <joshman211@gmail.com>
2022-05-14 18:13:47 -0500

    additional changes to styling

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 /*
-TODO: Determine how to style the divs so they are in a grid with flexbox
-TODO: Create a function that creates the divs on page load 16x16 grid each div class of grid with unique id
-TODO: Create a function that determines which grid you are hovering over and onclick() mark the grid / toggle a style black background
-TODO: The backfill color should be selectable 
-TODO: Grid size should be adjustable support up to 64
+TODO: Wrap styling buttons and colors
+TODO: Add footer
+TODO: Adjust container size on the fly to change as grid changes. IE Width of grid cells should scale
+
 */
 
 let color = 'black';
@@ -30,6 +29,9 @@ function createDivs(gridSize, container) {
       col.classList.add(`r${x}`);
       col.classList.add(`c${y}`);
       col.style.cssText = 'outline: 1px dashed #f90; width: 20px; height: 20px';
+      col.addEventListener('click',function(){
+          changeColor(this);
+      });
       row.appendChild(col);
     };
 
@@ -56,5 +58,12 @@ function resetDivs(){
 
 // change elements background color to global color currently set
 function changeColor(element) {
-    element.style.cssText = `background-color:${color}`;
+    //element.style.cssText = `background-color:${color};`;
+    element.style.backgroundColor = color;
+}
+
+function randomizeColor(){
+    const colors = ["aqua","blue","fuchsia","gray","green","lime","maroon","navy","olive","purple","red","silver","teal","yellow"];
+    let r = Math.floor((Math.random() * colors.length) + 1);
+    color = colors[r];
 }
